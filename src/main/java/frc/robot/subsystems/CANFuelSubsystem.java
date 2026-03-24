af9f9e915ea069ff0fc43dfc226c8016eceda47a
cronus314 <93542597+Cronus314@users.noreply.github.com>
2026-03-24 14:53:13 -0400

    fixed deprecated imports

--- src/main/java/frc/robot/subsystems/CANFuelSubsystem.java
+++ src/main/java/frc/robot/subsystems/CANFuelSubsystem.java
@@ -6,8 +6,8 @@ package frc.robot.subsystems;
 
 import static frc.robot.Constants.FuelConstants.*;
 
-import com.revrobotics.spark.SparkBase.PersistMode;
-import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkMaxConfig;
